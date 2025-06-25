382199e0cc1606c9e7016df8a7777e2ba070f8bf
Joshua <joshuaogaga2015@gmail.com>
2025-06-25 14:42:57 -0700

    new

--- app.py
+++ app.py
@@ -27,6 +27,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score
+import shutil
 import warnings
 warnings.filterwarnings('ignore')
 
